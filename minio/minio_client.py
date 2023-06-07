d4b74705f7f5f5566e81052491385df583293e6a
nikk <nikk@gmail.com>
2023-06-08 01:03:48 +0530

    added minio poc

---
+++ minio/minio_client.py
@@ -0,0 +1,44 @@
+from minio import Minio
+
+# Initialize the Minio client
+minio_client = Minio(
+    "localhost:31074",
+    access_key="FVb2LAu4offnrC2oJ3t2",
+    secret_key="jYCXEhNhNPMsjMtbiuEvz2GomKRYSx0FeRjMtUSF",
+    secure=False  # Set it to True if you're using HTTPS
+)
+
+# Create a new bucket
+def create_bucket(bucket_name):
+    if not minio_client.bucket_exists(bucket_name):
+        minio_client.make_bucket(bucket_name)
+        print(f"Bucket '{bucket_name}' created successfully.")
+    else:
+        print(f"Bucket '{bucket_name}' already exists.")
+
+# Upload a file to a bucket
+def upload_file(bucket_name, file_path, object_name):
+    minio_client.fput_object(bucket_name, object_name, file_path)
+    print(f"File '{file_path}' uploaded as '{object_name}'.")
+
+# Download a file from a bucket
+def download_file(bucket_name, object_name, file_path):
+    minio_client.fget_object(bucket_name, object_name, file_path)
+    print(f"File '{object_name}' downloaded to '{file_path}'.")
+
+# List objects in a bucket
+def list_objects(bucket_name):
+    objects = minio_client.list_objects(bucket_name, recursive=True)
+    for obj in objects:
+        print(obj.object_name)
+
+# Example usage
+if __name__ == "__main__":
+    bucket_name = "my-bucket"
+    object_name = "my-object"
+    file_path = "sample-file.txt"
+
+    create_bucket(bucket_name)
+    upload_file(bucket_name, file_path, object_name)
+    download_file(bucket_name, object_name, "downloads/downloaded_file.txt")
+    list_objects(bucket_name)
---
+++ minio/readme.md
@@ -0,0 +1,37 @@
+create a kuber netes cluster on either of the platform of your choice. Example:
+>Rancher desktop
+>Docker Desktop
+>AWS EKS etc
+
+I used Docker Desktop for this POC.
+
+Now apply minio.yaml using kubectl: (I have setup k as an alias for kubectl)
+>k apply -f minio.yaml
+
+This will create a new namespace called minio-ns and will create a deployment and a NodePort service inside it:
+>k get deploy -n minio-ns
+>k get pods -n minio-ns
+>k get svc -n minio-ns
+
+while checking service, note down the two nodePorts corresponding to mino pod ports 9090(31072 in my case) and 9000(31074 in my case).
+
+Use the NodePort corresponding to pod port 9090 to access Minio GUI on localhost:
+http://localhost:31072/
+
+Generate access-key and secret-key using this GUI in browser and use them in minio_client.py to access the hosted minio.
+
+We will be using "localhost:31074" as API endpoint in our minio client written in python code(note that I have used the NodePort corresponding to API port i.e 9000):
+localhost:31074
+
+Install python minio module in case not present:
+>pip install minio
+
+Now we can use out minio client to access minio to create bucket, upload file, download file, list buckets etc.
+
+We can also exec into the minio pod and check the "data" folder(which we had setup in minio.yaml)
+>k exec -it -n minio-dev minio-598b59589-vwp26 -- /bin/bash
+>ls
+>cd data
+ls
+
+it will have all our buckets and corresponding objects within them
