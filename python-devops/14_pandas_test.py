80bd0063720a5bedcbcaec7bd88127504e861c51
nikk0007 <85676202+nikk0007@users.noreply.github.com>
2023-06-12 15:14:19 +0530

    Create 14_pandas_test.py

---
+++ python-devops/14_pandas_test.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Create a DataFrame
+data = {'Name': ['Alice', 'Bob', 'Charlie'],
+        'Age': [25, 30, 35],
+        'City': ['New York', 'London', 'Paris']}
+df = pd.DataFrame(data)
+print(df)
+
+# Accessing data in a DataFrame
+print(df['Name'])                 # Access a specific column
+print(df.loc[0])                  # Access a specific row using label-based indexing
+print(df.iloc[1])                 # Access a specific row using integer-based indexing
+print(df.loc[0, 'Age'])            # Access a specific element using label-based indexing
+
+# Writing data to a CSV file
+df.to_csv('data.csv', index=False)
+
+# Reading data from a CSV file into a DataFrame
+df = pd.read_csv('data.csv')
+print(df)
