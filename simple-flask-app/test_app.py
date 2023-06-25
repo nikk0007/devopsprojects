b765ccdda449160868c9201a99d632c192df6e49
nikk <nikk@gmail.com>
2023-06-25 17:31:58 +0530

    added test case

--- simple-flask-app/test_app.py
+++ simple-flask-app/test_app.py
@@ -1,6 +1,4 @@
 from simple-python-flask-app import home
 
-# Sample test case
-def test_home_page():
-    response = home()
-    assert response == "Welcome to the home page!"
+# add your test cases below
+
